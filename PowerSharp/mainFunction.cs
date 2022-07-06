4b13c87ae8eca37c1d8b6befc72bafb65a6aa311
Lakshmi VidyaSagar <Lakshmi@thetrycatch.com>
2022-07-06 22:35:04 +0530

    adding testtable Main Class

---
+++ PowerSharp/mainFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workbench.PowerSharp;
+
+namespace PowerSharp
+{
+    public class mainFunction
+    {
+        public static void Main()
+        {
+            var b = new PSCode(Environment.MachineName, "get-service", CodeType.Script).Invoke();
+        }
+    }
+}
